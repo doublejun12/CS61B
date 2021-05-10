f10522c028dd1db6a732e30a2336390ad2e48884
doublejun12 <doublejun12@gmail.com>
2021-05-10 22:13:58 +0800

    finish hw1

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -12,23 +12,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     public int fillCount() {
         return fillCount;
     }
-
-    @Override
-    public abstract void enqueue(T x);
-
-    @Override
-    public abstract T dequeue();
-
-    @Override
-    public abstract T peek();
-
-    @Override
-    public boolean isEmpty() {
-        return fillCount() == 0;
-    }
-
-    @Override
-    public boolean isFull() {
-        return capacity() == fillCount();
-    }
 }
